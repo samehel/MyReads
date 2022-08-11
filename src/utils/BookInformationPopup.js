33ed045d08548c9bd6bdcd2eb3bbcc8162244b06
Sameh E <89010606+samehel@users.noreply.github.com>
2022-08-11 21:29:04 +0200

    Update BookInformationPopup.js

--- src/utils/BookInformationPopup.js
+++ src/utils/BookInformationPopup.js
@@ -2,8 +2,9 @@ import React from "react";
 
 /*
   Thanks to cluemediator.com.com for the help of implementing this extra feature (not required by udacity).
-  I have also asked for permission from my EgFwd Leader if it was okay and I was granted permission since 
-  it is an extra feature.
+  I have also asked for permission from my EgFwd Scholorship Leader if it was okay and I was granted permission since 
+  it is an extra feature. This extra feature also has implemented parts such as the popup box body information located
+  in Books.js and the css of the popup box located in App.css. 
 */
 
 const BookInformationPopup = props => {
